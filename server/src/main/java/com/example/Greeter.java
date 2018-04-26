62e0057b23da96a8da8b4b056a0cb6ff0dddbbfb
Hicham TAHIRI <tahirihta@gmail.com>
2018-04-26 16:04:10 +0200

    Checkstyle Warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,11 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+  * @param name of the person
+  * @retrun hello greetings
+  */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
